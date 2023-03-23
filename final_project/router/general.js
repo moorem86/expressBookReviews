7d16b01bf3d17e834872de9ddd2744790c4d3552
Megan <moorem86@gmail.com>
2023-03-23 12:17:05 -0400

    Complete final project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,18 +20,35 @@ public_users.post("/register", (req,res) => {
   return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
+// Get the book list available in the shop (sequential code)
 public_users.get('/',function (req, res) {
     res.send(JSON.stringify(books,null,4));
   });
 
-// Get book details based on ISBN
+  // Get book list (using promise)
+  public_users.get('/books',function (req, res) {
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+      get_books.then(() => console.log("Promise for Task 10 resolved"));
+  });
+
+// Get book details based on ISBN (sequential code)
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn=req.params.isbn 
     res.send(books[isbn]);
   });
   
-// Get book details based on author
+  // Get book details based on ISBN (using promise)
+public_users.get('/books/isbn/:isbn',function (req, res) {
+    const isbn=req.params.isbn 
+    let get_book_isbn= new Promise((resolve, reject)=>{
+        resolve(res.send(books[isbn]));
+    })
+    get_book_isbn.then(()=>{console.log("Task 11 successful")})
+  });
+
+// Get book details based on author (sequential code)
 public_users.get('/author/:author',function (req, res) {
   const author=req.params.author
 
@@ -42,23 +59,48 @@ public_users.get('/author/:author',function (req, res) {
       }
   }
   return res.status(300).json({message: "Author not in list"});
-
-
-  
 });
 
-// Get all books based on title
+//get  books based on author (using promise)
+public_users.get('/books/author/:author',  (req, res)=> {
+    const author = req.params.author
+    let books_w_author = []
+    let get_book_author = new Promise((resolve, reject)=> {
+        for (book in books){
+            if (books[book].author === author){
+                books_w_author.push(books[book])
+            }
+        }
+        resolve(res.status(200).send(books_w_author))
+    })
+    get_book_author.then(() => console.log("Task 12 successful"))
+});
+
+
+// Get all books based on title (sequential code)
 public_users.get('/title/:title',function (req, res) {
     const title=req.params.title
-
-  for (book in books){
-     
+    for (book in books){
       if (books[book].title===title){
           res.send(books[book])
       }
   }
   return res.status(300).json({message: "Title not in list"});
+});
 
+//get books based on title (using promise)
+public_users.get('/books/title/:title', (req, res) =>{
+    let title = req.params.title
+    let books_w_title=[]
+    let title_promise=new Promise((resolve, reject) =>{
+        for (book in books){
+            if (books[book].title===title){
+                books_w_title.push(books[book])
+            }
+        }
+        resolve(res.status(200).send(books_w_title))
+    })
+    title_promise.then(()=>console.log("Task 13 successful"))
 });
 
 //  Get book review
